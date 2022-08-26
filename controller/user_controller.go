5b41d182562120681a7dde29985bde2f791d6480
yomaisch <apricotraisin1024@gmail.com>
2022-08-26 15:08:37 +0900

    Add error handle

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -13,14 +13,13 @@ func NewUserController() *UserController {
 	return &UserController{}
 }
 
-// echo.Get()メソッドの第2引数にはecho.HandlerFuncでないとコンパイルエラーになるため、
-// 本来のコントローラメソッドをラップしている。
+// echo.Get()メソッドの第2引数にはecho.HandlerFuncでないとコンパイルエラーになるため、本来のコントローラメソッドをラップしている。
 func (uc *UserController) GetUser() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		user := new(User)
-		// if err := c.Bind(user); err != nil {
-		// 	return err
-		// }
+		if err := c.Bind(user); err != nil {
+			return err
+		}
 		return c.JSON(http.StatusOK, user)
 	}
 }
